06cff77e05c1b59cf18cb40589342d752b920eb3
Slasky <sv.pero@gmail.com>
2018-11-30 16:25:31 +0200

    add logging done ch 7 (email sender dosent work)

--- app/forms.py
+++ app/forms.py
@@ -31,3 +31,13 @@ class EditProfileForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     about_me = TextAreaField('About me', validators=[Length(min=0, max=140)])
     submit = SubmitField('Submit')
+
+    def __init__(self, original_usernam, *args, **kwargs):
+        super(EditProfileForm, self).__init__(*args, **kwargs)
+        self.original_usernam = original_usernam
+
+    def validate_username(self, username):
+        if username.data != self.original_usernam:
+            user = User.query.filter_by(username=self.username.data).first()
+            if user is not None:
+                raise ValidationError('Pliase use a different usernamename')
